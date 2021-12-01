14d6b001133b96e356c55191ce7e07482c9fa4c5
MrV <mrv777@users.noreply.github.com>
2021-12-01 17:19:14 -0600

    Better leaderboard layout

--- src/components/Leaderboards.tsx
+++ src/components/Leaderboards.tsx
@@ -32,16 +32,15 @@ const Leaderboards = (): ReactElement => {
   return (
     <div className="h-full w-full flex flex-col items-center justify-center">
       <p className="text-2xl">Leaderboards</p>
-      <div className="w-full grid grid-cols-3 gap-4">
-        <div className="underline">Name</div>
-        <div className="underline">Team</div>
-        <div className="underline">Score</div>
+      <div className="w-full grid grid-cols-4 gap-4">
+        <div className="underline col-span-3">Name</div>
+        <div className="underline text-right">Score</div>
         {leaders
           .sort((a, b) => (a.value < b.value ? 1 : -1))
           .map((leader, index) => (
-            <div className="grid grid-cols-3 col-span-3" key={leader.recipient}>
-              <div className="col-start-1 col-end-1">{leader.recipientRS}</div>
-              <div className="col-start-3 col-end-3">{leader.value}</div>
+            <div className="grid grid-cols-4 col-span-4" key={leader.recipient}>
+              <div className="col-start-1 col-end-4">{leader.recipientRS}</div>
+              <div className="col-start-4 col-end-4 text-right">{leader.value}</div>
             </div>
           ))}
       </div>
