3cf6368fb836a1125b3239d8e8a05417e9e230a6
MrV <mrv777@users.noreply.github.com>
2021-12-18 17:25:17 -0600

    Add assets to context

--- src/contexts/playerContext.ts
+++ src/contexts/playerContext.ts
@@ -7,6 +7,8 @@ export const PlayerContext = createContext<PlayerContextType>({
   updatePlayerAccount: () => null,
   playerAccount: null,
   updatePlayerStatus: () => null,
+  playerAssets: null,
+  updatePlayerAssets: () => null,
   playerStatus: null,
   signOut: () => null,
 });
