741236d19c630f99d2696a2c6eb4ff88eaafa6b1
MrV <mrv777@users.noreply.github.com>
2021-12-20 15:14:41 -0600

    Add PWA icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,34 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), VitePWA()],
+  plugins: [
+    reactRefresh(),
+    VitePWA({
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Crypto Heroes',
+        short_name: 'CryptoHeroes',
+        description: 'Be the hero to lead your blockchain to victory',
+        theme_color: '#1e1e1e',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
